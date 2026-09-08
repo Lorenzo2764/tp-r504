ea5a91068acb73fc5f462c425e0705adf424d8db
Lorenzo <Lorenzomartine64@gmai.com>
2026-09-08 17:59:44 +0200

    mise a jour fonctions, prog1 et test

--- TP1/fonctions.py
+++ TP1/fonctions.py
@@ -1,12 +1,15 @@
 def puissance(a,b):
-	if not type(a) is int:
-		raise TypeError("Nombre entiers seulement")
-	if not type(b) is int: 
-		raise TypeError("Nombre entiers seulement")
+	if not type(a) is int or not type(b) is int:
+		raise TypeError("Nombres entiers seulement")
+
+	if a == 0 and b < 0:
+		return ("0^"+str(b)+" est indéfini.")
+
+	res = a
+
+	for i in range(1,abs(b)):
+		res *= a
+
 	if b < 0:
-		raise Exception("Exposant negatif non supporte")
-	
-	resultat = 1
-	for i in range(b):
-		resultat = resultat * a
-	return resultat
+		return 1/res
+	return res
